97873dcfe3f58c22623d329aec76fe79d5cf6e46
Cem Bektasoglu <cembektasoglu7@gmail.com>
2024-11-30 20:21:08 +0100

    Enemy behaviour

---
+++ Assets/Cem/Scripts/EnemySlime.cs
@@ -0,0 +1,26 @@
+﻿public class EnemySlime : EnemyBase
+{
+
+
+
+    protected override void Update()
+    {
+        base.Update();
+    }
+    protected override void Attack()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void Die()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void MoveCharacter()
+    {
+        throw new System.NotImplementedException();
+    }
+}
+
+
---
+++ Assets/Cem/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public enum GameState
+{
+    Started,
+    Playing,
+    GameEnd,
+    Paused,
+    Resume,
+}
+
+public class GameManager : MonoBehaviour
+{
+    // Singleton instance
+    public static GameManager Instance { get; private set; }
+
+    // Game state variables
+    public GameState GameState { get; set; }
+
+    private void Awake()
+    {
+        // Singleton pattern: Ensure only one instance exists
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+        GameState = GameState.Started;
+    }
+
+
+
+}
+
