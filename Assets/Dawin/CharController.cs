4e5948b5cd60b9f4f7cff315fd6b3a39a729e344
Dawin <dawinyurtseven@gmail.com>
2024-11-30 19:48:25 +0100

    short push with sprites

--- Assets/Dawin/CharController.cs
+++ Assets/Dawin/CharController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 using Random = UnityEngine.Random;
 
 public class CharController : MonoBehaviour
@@ -18,9 +19,18 @@ public class CharController : MonoBehaviour
 
     #region components
 
+    public Animator anim;
+    public SpriteRenderer sprite;
     public GameObject anchor;
     public Camera cam;
     public GameObject gun;
+    public GameObject rememberScreen;
+    public GameObject[] images;
+
+    public RawImage[] first;
+    public RawImage[] second;
+    public RawImage[] third;
+    public RawImage[] fourth;
 
     #endregion
 
@@ -59,6 +69,10 @@ public class CharController : MonoBehaviour
     {
         while (true)
         {
+            while (LobotomyTime)
+            {
+                yield return null;
+            }
             yield return new WaitForSeconds(45f);
             var counter = 0f;
             while (counter < 15f)
@@ -71,10 +85,40 @@ public class CharController : MonoBehaviour
                 var chance = Random.Range(0.0f, 1.0f);
                 if (chancesOfLobotomy[i] > chance)
                 {
+                    LobotomyTime = true;
                     lockedElement = i;
                     UIHandler.instance.lockElement(i, lobotomyDuration);
+                    StartCoroutine(KeepLobotomy(i));
                 }
             }
+        }
+    }
+
+    private IEnumerator KeepLobotomy(int index)
+    {
+        yield return new WaitForSeconds(lobotomyDuration);
+        //Slow down time for the mini game
+        Time.timeScale = 0.1f;
+        //mini game
+        StartCoroutine(Remember(index));
+    }
+
+    private IEnumerator Remember(int i)
+    {
+        rememberScreen.SetActive(true);
+        var index = Random.Range(1, 3);
+        for (int j = 0; j < 3; j++)
+        {
+            if (index == j)
+            {
+                
+            }
+        }
+        var counter = 0f;
+        while (counter < 5f)
+        {
+            counter += Time.deltaTime;
+            yield return null;
         }
     }
 
@@ -108,6 +152,16 @@ public class CharController : MonoBehaviour
         if (context.performed)
         {
             moveInput = context.ReadValue<Vector2>();
+            if (moveInput.x < 0)
+            {
+                sprite.flipX=true;
+            }
+            else if (moveInput.x > 0)
+            {
+                sprite.flipX = false;
+            }
+            anim.SetFloat("U/D", moveInput.y);
+            anim.SetFloat("L/R", moveInput.x);
             dodgeInput = moveInput;
         }
 
@@ -154,7 +208,6 @@ public class CharController : MonoBehaviour
             }
 
             StartCoroutine(projectiles[projectileIndex].CoolDown());
-            Debug.Log("Pew!");
             projectile shot = projectiles[projectileIndex];
             float angle = CalcDir();
             shot.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
@@ -164,6 +217,7 @@ public class CharController : MonoBehaviour
             {
                 //Fire
                 case 0:
+                    chancesOfLobotomy[0] += increaseRate;
                     float spacing = 0.0f;
                     for (int i = 0; i < 50; i++)
                     {
@@ -173,20 +227,22 @@ public class CharController : MonoBehaviour
                         float speedOffset = Random.Range(-4f, 4f);
                         shots.speed = 15.0f + speedOffset;
                         spacing = 30f / 50f * i;
-                        Debug.Log(shot.transform.rotation.eulerAngles.z);
                     }
 
                     break;
                 //Lightning
                 case 1:
+                    chancesOfLobotomy[1] += increaseRate;
                     Instantiate(shot);
                     break;
                 //Earth
                 case 2:
+                    chancesOfLobotomy[2] += increaseRate;
                     Instantiate(shot);
                     break;
                 //Water
                 case 3:
+                    chancesOfLobotomy[3] += increaseRate;
                     StartCoroutine(ShootWater(shot));
                     break;
             }
@@ -209,7 +265,6 @@ public class CharController : MonoBehaviour
     {
         if (context.started)
         {
-            //Debug.Log(context.ReadValue<Single>());
             var side = context.ReadValue<Single>();
             if (side > 0)
             {
